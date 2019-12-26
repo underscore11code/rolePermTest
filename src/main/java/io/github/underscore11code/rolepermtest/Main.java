f7d45346bb07bd96a062e66abcf211accd39ad49
underscore11code <minecrafter11mrt@gmail.com>
2019-12-26 08:53:13 -0800

    Initial commit

---
+++ src/main/java/io/github/underscore11code/rolepermtest/Main.java
@@ -0,0 +1,28 @@
+package io.github.underscore11code.rolepermtest;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.server.Server;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your bot's token:");
+        String token = scanner.nextLine();
+        System.out.println("Logging into Discord...");
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        System.out.println("Logged in!");
+        for (Server server : api.getServers()) {
+            System.out.println("G: " + server.getName() + "(" + server.getIdAsString() + ")");
+            server.getAllowedPermissions(api.getYourself()).forEach(permissionType -> System.out.println("  " + permissionType.name()));
+            for (ServerTextChannel channel : server.getTextChannels()) {
+                System.out.println("  TC: " + channel.getName() + "(" + channel.getIdAsString() + ")");
+                channel.getEffectiveAllowedPermissions(api.getYourself()).forEach(permissionType -> System.out.println("    " + permissionType.name() + ", ALLOWED"));
+                channel.getEffectiveDeniedPermissions(api.getYourself()).forEach(permissionType -> System.out.println("    " + permissionType.name() + ", DENIED"));
+            }
+        }
+    }
+}
